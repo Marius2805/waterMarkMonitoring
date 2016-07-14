64bbd94bca0e4f26be3a83bbb48c648e483a703a
Marius Meißner <marius.meissner@googlemail.com>
2016-07-14 22:53:02 +0200

    Dev: Added ChartJS 2.0 RC1 library

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,5 +5,6 @@
     <link rel="stylesheet" href="/css/semantic.min.css" type="text/css">
     <link rel="stylesheet" href="/css/style.css" type="text/css">
     <script language="javascript" type="text/javascript" src="/js/jquery-3.1.0.min.js"></script>
+    <script language="javascript" type="text/javascript" src="/js/Chart.bundle.min.js"></script>
     <script language="javascript" type="text/javascript" src="/js/semantic.min.js"></script>
 </head>
